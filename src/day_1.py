740cac32c52160dc1340b7a7949cc6da56fdf261
JP Dhabolt <john.p.dhabolt@gmail.com>
2019-12-24 11:17:04 -0500

    Completed Day 1

--- src/day_1.py
+++ src/day_1.py
@@ -6,3 +6,14 @@ def calculate_fuel(masses: List) -> int:
     for mass in masses:
         fuel += int(mass / 3) - 2
     return fuel
+
+
+def calculate_actual_fuel(masses: List) -> int:
+    fuel = 0
+    for mass in masses:
+        interim_fuel = int(mass / 3) - 2
+        additional_fuel = (
+            0 if interim_fuel < 6 else calculate_actual_fuel([interim_fuel])
+        )
+        fuel += interim_fuel + additional_fuel
+    return fuel
--- tests/test_day_1.py
+++ tests/test_day_1.py
@@ -1,6 +1,7 @@
 from pytest import mark
 
 from .resources import inputs
+from src.day_1 import calculate_actual_fuel
 from src.day_1 import calculate_fuel
 
 
@@ -9,8 +10,18 @@ from src.day_1 import calculate_fuel
 )
 def test_calculate_fuel_with_provided(test_input, expected):
     actual = calculate_fuel([test_input])
-    assert expected == actual
+    assert actual == expected
 
 
 def test_calculate_fuel_for_answer():
     print(calculate_fuel(inputs.advent_1_input))
+
+
+@mark.parametrize("test_input,expected", [(14, 2), (1969, 966), (100756, 50346)])
+def test_calculate_actual_fuel_with_provided(test_input, expected):
+    actual = calculate_actual_fuel([test_input])
+    assert actual == expected
+
+
+def test_calculate_actual_fuel_for_answer():
+    print(calculate_actual_fuel(inputs.advent_1_input))
